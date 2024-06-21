8d676a62fb881a64823cf04288e341dbf976ac3f
Ruzindana Kent <ruzindanakent.rk@gmail.com>
2024-06-21 17:41:30 +0200

    basic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,16 @@
+
+
+
+number1 = 10
+
+number2 = 5
+
+
+tot = number1 + number2
+print("Addition of 10 and 5 is ", tot)
+
+diff = number1 - number2
+print("Substraction of 10 and 5 is ", diff)
+
+prod = number1 * number2
+print("Multiplication of 10 and 5 is ", prod)
