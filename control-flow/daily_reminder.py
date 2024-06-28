b83aeae37f5a7d7e791536f7445e600763fa185b
Ruzindana Kent <ruzindanakent.rk@gmail.com>
2024-06-28 15:54:01 +0200

    daily reminder

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,39 @@
+
+
+#### Pseudo code ####
+
+# BEGIN
+
+# INPUT: task, priority, time_bound
+# match priority:
+#       case 'high':
+#           OUTPUT: "task is a high priority"
+#       case 'medium':
+#           OUTPUT: "task is a meium priority"
+#       case 'low':
+#           OUTPUT: "task is a low priority"
+# END
+
+
+task = input("Enter your task: ")
+priority = input("Priority (high/medium/low): ")
+time_bound = input("Is it time-bound (yes/no): ")
+
+match priority:
+    case 'high':
+        if time_bound == 'yes':
+            print(f"'{task}' is a high priority task that requires you immediate attention today ")
+        else:
+            print(f"'{task}' is a high priority task. Consider completing it when you have free time")
+    case 'medium':
+        if time_bound == 'yes':
+            print(f"'{task}' is a meium priority task that requires you immediate attention today ")
+        else:
+            print(f"'{task}' is a meium priority. Consider completing it when you have free time")
+    case 'low':
+        if time_bound == 'yes':
+            print(f"'{task}' is a low priority task that requires you immediate attention today ")
+        else:
+            print(f"'{task}' is a low priority. Consider completing it when you have free time")
+    case _:
+        print("Invalid input")
