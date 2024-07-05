2dfaa6fe0f46f1ab812625e148ea9d150e3c1f85
Ruzindana Kent <ruzindanakent.rk@gmail.com>
2024-07-05 15:54:30 +0200

    function arithmetic_operation updated

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,20 +1,16 @@
 # Python script for Arithmetic operations
 
 def perform_operation(num1, num2, operation):
-    '''match the input operation'''
-    match operation:
-        case operation if operation == 'add':
+    '''match the input operation with elif'''
+         if operation == 'add':
             return num1 + num2
-        case operation if operation == 'subtract':
+         elif operation == 'subtract':
             return num1 - num2
-        case operation if operation == 'multiply':
+         elif operation == 'multiply':
             return num1 * num2
-        case operation if operation == 'divide':
-            if num2 != 0:
-                return num1/num2
-            else:
-                return "no number is divisible by zero"
-        case _:
-            return 'Invalid operation'
+         elif operation == 'divide' and num2 != 0:
+            return num1/num2
+         else:
+            return "invalid operation input"
 
 
