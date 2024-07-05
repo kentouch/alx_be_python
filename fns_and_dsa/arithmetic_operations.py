cbcf6f5c52bdc8f3c4f1bbc04a34cf3fdaaf4b81
Ruzindana Kent <ruzindanakent.rk@gmail.com>
2024-07-05 15:49:03 +0200

    function arithmetic_operation updated

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -12,6 +12,8 @@ def perform_operation(num1, num2, operation):
         case operation if operation == 'divide':
             if num2 != 0:
                 return num1/num2
+            else:
+                return "no number is divisible by zero"
         case _:
             return 'Invalid operation'
 
