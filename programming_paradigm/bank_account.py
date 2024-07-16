ab7a014679aa2105839d6385bf5944f55ebd86de
Ruzindana Kent <ruzindanakent.rk@gmail.com>
2024-07-16 06:14:33 +0200

    programming paradigm

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,28 @@
+### implementing a banking account class encapsulating banking operations
+# creates a class called BankingAccount
+# initialize the account balance
+# implementation of methods such deposit(), withdraw(), show_balance()
+
+class BankAccount:
+    # initialize he 
+    def __init__(self, account_balance):
+        self._account_balance = account_balance
+
+    # implement a deposit method using a parameter called amount
+    def deposit(self, amount):
+        # add the amount to the account balance
+        self._account_balance = self._account_balance + amount
+    
+    # implement a withdraw method using a parameter called amount
+    def withdraw(self, amount):
+        if amount > self._account_balance:
+            return False
+        else:
+            # subtract the amount from the account balance
+            self._account_balance = self._account_balance - amount
+            return True
+
+    # implement a display_balance method using a parameter called amount
+    def display_balance(self):
+        print(f"Your balance is :{self._account_balance}") 
+
