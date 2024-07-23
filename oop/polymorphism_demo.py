78f8866d4270034ce5dc65807f4e195501ece032
Ruzindana Kent <ruzindanakent.rk@gmail.com>
2024-07-23 15:57:55 +0200

    Polymorphism principles

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,21 @@
+### define a base class called "Shape", with a method area that raise an error "NotImplementError"
+# We will define also derived classes as rectangle and circle that will overide this area method
+
+
+import math
+class Shape:
+    def area(self): 
+        raise NotImplementedError
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+    def area(self):
+        return self.length * self.width
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+    def area(self):
+        return math.pi * self.radius * self.radius
