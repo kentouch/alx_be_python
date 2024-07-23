3020590edc46f2bdd96425d7488bcc73173a54e1
Ruzindana Kent <ruzindanakent.rk@gmail.com>
2024-07-23 15:27:46 +0200

    oop principles: inheritance and composition

---
+++ oop/library_system.py
@@ -0,0 +1,47 @@
+### class Book implements __init__ (constructor), 
+# with atttributes title and author,
+# and we build Ebook and PrintedBook classes inherites the Book class
+# we have library system that implements Book, PrintedBook and Ebook classes
+
+
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+# Ebook class derived from Book class
+class EBook(Book):
+    def __init__(self, title, author, file_size):
+        super().__init__(title, author)
+        self.file_size = file_size
+
+# Printbook class derived from Book class
+class PrintBook(Book):
+    # initialize attributes from base class __init__
+    def __init__(self, title, author, page_count):
+        super().__init__(title, author)
+        self.page_count = page_count
+    
+# composition Library class
+class Library:
+    # initialize a list to store instance of Books, Ebooks and PrintBooks
+    def __init__(self):
+        self.books = []
+
+    # add book instance method to add books in the list
+    def add_book(self, book):
+        self.book = book
+        self.books.append(self.book)
+    
+    # list all the book in the library using list_books method
+    def list_books(self):
+        for b in self.books:
+            if b.__class__ == Book:
+                print(f"Book: {b.title} by {b.author}")
+            elif b.__class__ == PrintBook:
+                print(f"PrintBook: {b.title} by {b.author}, file size: {b.page_count}")
+            else:
+                print(f"EBook: {b.title} by {b.author}, page_count: {b.file_size}")
+            
+            
+            
+            
