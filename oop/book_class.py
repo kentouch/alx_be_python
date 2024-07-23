ae48a39e75156e0920c0213bb910b71a3d2d5761
Ruzindana Kent <ruzindanakent.rk@gmail.com>
2024-07-23 09:49:19 +0200

    oop principles: str, repr and destructors

---
+++ oop/book_class.py
@@ -0,0 +1,21 @@
+### Create a book class that implements __init__ (constructor), __del__ (destructor), __str__ (string representation) methods
+## and __repr__ (official string representation)
+
+class Book:
+    # constructor
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+
+    # destructor
+    def __del__(self):
+        print(f"Deleting {self.title}")
+
+    # string representation
+    def __str__(self):
+        return f"{self.title} by {self.author}, published in ({self.year})"
+
+    # official string representation
+    def __repr__(self):
+        return f"Book('{self.title}', '{self.author}', {self.year})"
